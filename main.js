3176c0a9ae07cea06db7537914cd9a095a143120
Sakurai <dnsn.isk@gmail.com>
2015-11-22 13:42:33 +0900

    Implementing drawer.js.

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-//==== snap.js ======
-// 基本的な記述です。コンテンツ部分のID名を書いておきましょう。
-var snapper = new Snap({
-    element: document.getElementById('slide'), //この部分が動きます
-    disable: 'left',
-    minPosition: -200,
-    transitionSpeed: 0.4
-});
- 
-// 以下はフリックやドラッグだけでなく、ボタンでも開いたり閉じたりできるようにする記述です。
-var addEvent = function addEvent(element, eventName, func) {
-    if (element.addEventListener) {
-        return element.addEventListener(eventName, func, false);
-    } else if (element.attachEvent) {
-        return element.attachEvent("on" + eventName, func);
-    }
-};
- 
-//==呼びたすボタン== ボタンのID名と揃えておきましょう。 
-addEvent(document.getElementById('open-right'), 'click', function(){
-    snapper.open('right')
+//===== headroom.jp =====
+$(document).ready(function () {
+    $('.drawer').drawer();
 });
 
 
 //===== headroom.jp =====
-var target = document.getElementById("base");
+var target = document.querySelector("header");
 var headroom  = new Headroom(target);
 headroom.init();
+
