b054c2cbab9d7a49bf3ede37a466b7ae4368461c
Sakurai <dnsn.isk@gmail.com>
2015-11-28 01:49:18 +0900

    不要なファイルを整理、削除

--- api1.js
+++
@@ -1,9 +0,0 @@
-var url = "http://api.openweathermap.org/data/2.5/weather?q=London";
-
-jQuery.getJSON(url, function(data){
-    alert(url);
-});
-
-var elem = documet.getElementById("temp_max");
-
-
